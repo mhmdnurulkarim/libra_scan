6b5d9e0c855c173ddb99e56a5281d69703f3c5e0
Muhammad Nurul Karim <karimmitra0@gmail.com>
2025-08-30 19:43:03 +0700

    chore: stop tracking ignored files

--- android/app/src/main/kotlin/com/mhmdnurulkarim/librascan/libra_scan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mhmdnurulkarim.librascan.libra_scan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
-
-String formatDate(Timestamp timestamp) {
-  final dateTime = timestamp.toDate();
-  return DateFormat('dd MMM yyyy').format(dateTime);
-}
